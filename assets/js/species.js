cc2fcca3d2beef4ca81cce52d1d74840159170df
Poupiio <elise.maaiza33@gmail.com>
2023-09-04 22:23:20 +0200

    ajout pages + cards espèces

---
+++ assets/js/species.js
@@ -0,0 +1,15 @@
+const mammals = document.querySelector('#mammals');
+const birds = document.querySelector('#birds');
+const reptils = document.querySelector('#reptils');
+
+function mammalsPage() {
+    window.location.href = "mammals.html";
+}
+
+function birdsPage() {
+    window.location.href = "birds.html";
+}
+
+function reptilsPage() {
+    window.location.href = "reptils.html";
+}
