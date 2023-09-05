2d0f531f872db2c00978573b0d510c27c6244501
Poupiio <elise.maaiza33@gmail.com>
2023-09-05 17:33:43 +0200

    remise header comme avant + box-shadow

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,17 +1,7 @@
 const discover = document.querySelector('#discover-species');
 const safariChoice = document.querySelector('#safari-choice');
 const logo = document.querySelector('.main-logo');
-const header = document.querySelector('header');
-const menu = document.querySelector('.hidden-menu');
-console.log(menu);
 
-header.addEventListener('mouseenter', () => {
-    menu.classList.remove('hidden-menu');
-    menu.classList.add('visible-menu');
-})
-header.addEventListener('mouseleave', () => {
-    menu.classList.add('hidden-menu');
-})
 
 // Fonctions de redirection vers les pages des espèces et des safaris
 function redirectToSpeciesPage() {
@@ -32,4 +22,5 @@ let swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
+    loop: true
 });
